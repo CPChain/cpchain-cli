d45488876dcf5e7cfa797eb3c4011794433ce2c4
zgljl2012 <2693491512@qq.com>
2022-03-23 12:52:29 +0800

    packaging with webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename)
+
+export default {
+  target: 'node',
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.cjs',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
