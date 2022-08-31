d409ba1fadbe1ba6a97ed2160cb9a8f8e28e9326
zgljl2012 <2693491512@qq.com>
2022-09-01 04:00:30 +0800

    compile and store bytecode

--- src/commands/compile/index.ts
+++ src/commands/compile/index.ts
@@ -4,27 +4,48 @@ import {
 } from '../../options'
 import { createLoader, createSolcDockerCompiler } from '../../core'
 import path from 'path'
+import utils from '../../utils'
+import fs from 'fs'
 
-function compile (filePath: string) {
+interface CompileProps {
+  filePath: string
+  output: string
+}
+
+function compile (props: CompileProps) {
+  const { output, filePath } = props
   const loader = createLoader()
   const dir = loader.load(filePath)
   const compiler = createSolcDockerCompiler({})
   const entryPath = path.join(process.cwd(), dir.dir, dir.entry)
   const results = compiler.compile(entryPath)
-  console.log(results)
+  utils.logger.info(`Your contracts will outoput to '${output}'`)
+  if (!fs.existsSync(output)) {
+    fs.mkdirSync(output)
+  }
+  for (const c of results) {
+    utils.logger.info(`Compile ${c.contractName}...`)
+    fs.writeFileSync(path.join(output, `${c.contractName}.json`), JSON.stringify(c, null, '  '))
+  }
+  utils.logger.info('End.')
+}
+
+const OutputDirOption = {
+  name: 'outputDir',
+  description: 'Output directory',
+  defaultValue: 'build'
 }
 
 export const compileCommand = (program: Command) => {
   const cmd = program
-    .command('compile')
-    .argument('<input-file>')
+    .command('compile <input-file>')
     .description('Compile solidity file')
 
   const myCommander = new MyCommander(cmd)
-  myCommander.useConfig()
+  myCommander.addOption(OutputDirOption).useConfig()
 
   // actions
-  myCommander.action(async (options) => {
-    compile(options)
+  cmd.action(async (name, options) => {
+    compile({ filePath: name, output: options.outputDir })
   })
 }
